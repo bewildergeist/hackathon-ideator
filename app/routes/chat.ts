4e549d81dcbc80489803ba6933cdddd3328ca748
Dan Okkels Brendstrup <dob@eaaa.dk>
2025-02-12 13:45:09 +0100

    feat: read user message from request body

--- app/routes/chat.ts
+++ app/routes/chat.ts
@@ -1,12 +1,18 @@
 import mistral from "../config/mistral.server";
+import type { Route } from "./+types/chat";
 
-export async function action() {
+export async function action({ request }: Route.ActionArgs) {
+  const { message } = await request.json();
   const response = await mistral.chat.complete({
     model: "mistral-small-latest",
     messages: [
       {
+        role: "system",
+        content: "Always respond with short and concise messages.",
+      },
+      {
         role: "user",
-        content: "Come up with good ideas for a hackathon project",
+        content: message,
       },
     ],
   });
