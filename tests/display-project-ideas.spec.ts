27dd18c267f9e76f6fefa27a0bb71e86aeb7c9df
Dan Okkels Brendstrup <dob@eaaa.dk>
2025-02-20 13:37:20 +0100

    feat: verify that all projects in database are shown

--- tests/display-project-ideas.spec.ts
+++ tests/display-project-ideas.spec.ts
@@ -1,20 +1,32 @@
 import { test, expect } from "@playwright/test";
+import mongoose from "mongoose";
+import ProjectIdea from "~/models/ProjectIdea";
 
-// TODO: This runs against the connected database, and should be updated to run
-// against a test database:
-// https://playwright.dev/docs/test-global-setup-teardown
-test("has correct number of project ideas", async ({ page }) => {
+test.beforeAll(async () => {
+  const connectionString = process.env.MONGODB_URI;
+  if (!connectionString) {
+    throw new Error("MONGODB_URI is not set");
+  }
+  mongoose.connect(connectionString);
+});
+
+test("shows all project ideas from the database", async ({ page }) => {
+  const projectIdeaCount = await ProjectIdea.countDocuments();
+  // Find and sort the project idea titles similar to how they are fetched in
+  // the home.tsx route:
+  const projectIdeaTitles = (
+    await ProjectIdea.find({}, { projectName: 1, _id: 0 }).sort({
+      createdAt: -1,
+    })
+  ).map((idea) => idea.projectName);
+
   await page.goto("/");
 
-  // Expect exactly 6 article elements with the correct headings
-  await expect(page.getByRole("article")).toHaveCount(6);
-  await expect(page.getByRole("heading", { level: 2 })).toHaveCount(6);
-  await expect(page.getByRole("heading", { level: 2 })).toHaveText([
-    "GroupNameGenie",
-    "AI-Powered Fitness Coach",
-    "AI Art Mentor",
-    "AI Home Design Assistant",
-    "Beach Cabin Concierge",
-    "AI-Driven Travel Planner",
-  ]);
+  // Expect to see an <article> and <h2> for each project idea in the database.
+  await expect(page.getByRole("article")).toHaveCount(projectIdeaCount);
+
+  const projectIdeaHeadings = page.getByRole("heading", { level: 2 });
+
+  await expect(projectIdeaHeadings).toHaveCount(projectIdeaCount);
+  await expect(projectIdeaHeadings).toHaveText(projectIdeaTitles);
 });
